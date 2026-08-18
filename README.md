10cd7d05a4653abfe7e9601cf38ceadffc25057f
leosturmer <leo@leo.com>
2026-08-18 18:40:44 -0300

    mudanças e readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Site agregador de links com Tailwind
+
+Projeto para aprender a utilização do Tailwind CSS. A ideia foi fazer uma página simples, semelhante a agregadores de links como LinkTree. Utilizei o Javascript para adicionar o estilo nos botões, para evitar a repetição interna no código.
+
+![Captura de tela do site](site_links.png)
--- styles.js
+++ styles.js
@@ -1,6 +1,7 @@
 const estilos = {
   linkPrincipal:
-    "font-mono text-black font-medium text-xl p-3 m-3 min-w-80 bg-emerald-600 border-black border-2 rounded-full shadow-[4px_4px_0px_0px_#000]  hover:bg-green-300 hover:text-black hover:underline hover:shadow-[4px_4px_0px_0px_#000]",
+    "font-mono text-black font-medium text-xl p-3 m-3 min-w-80 bg-emerald-600 border-black border-2 rounded-full shadow-[4px_4px_0px_0px_#000]  hover:bg-green-300 hover:text-black hover:shadow-[4px_4px_0px_0px_#000] text-center",
+  
 };
 
 document.addEventListener("DOMContentLoaded", () => {
